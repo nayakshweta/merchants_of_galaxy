2ca8caa49856f67f6f202e5b9b4d7cb7c9a4a3af
shweta <nayakshweta07@gmail.com>
2019-03-29 12:31:47 +0530

    Adds method to convert from roman to arabic.

--- tests/test_tradebook.py
+++ tests/test_tradebook.py
@@ -9,3 +9,38 @@ class TestTradeBook(TestCase):
         assert tradebook.traded_items.has_key('Silver')
         assert type(tradebook.traded_items['Silver']) is dict
         assert tradebook.traded_items['Silver']['value_in_alien'] == None
+
+    def test_convert_from_roman_to_arabic_I(self):
+        tradebook = TradeBook()
+        value_in_roman = 'I'
+        value_in_arabic = tradebook.convert_from_roman_to_arabic(value_in_roman)
+
+        assert value_in_arabic == 1
+    
+    def test_convert_from_roman_to_arabic_VI(self):
+        tradebook = TradeBook()
+        value_in_roman = 'VI'
+        value_in_arabic = tradebook.convert_from_roman_to_arabic(value_in_roman)
+
+        assert value_in_arabic == 6
+
+    def test_convert_from_roman_to_arabic_MCMXLIV(self):
+        tradebook = TradeBook()
+        value_in_roman = 'MCMXLIV'
+        value_in_arabic = tradebook.convert_from_roman_to_arabic(value_in_roman)
+
+        assert value_in_arabic == 1944
+
+    def test_convert_from_roman_to_arabic_XXXIX(self):
+        tradebook = TradeBook()
+        value_in_roman = 'XXXIX'
+        value_in_arabic = tradebook.convert_from_roman_to_arabic(value_in_roman)
+
+        assert value_in_arabic == 39
+    
+    def test_convert_from_roman_to_arabic_MDCL(self):
+        tradebook = TradeBook()
+        value_in_roman = 'MDCL'
+        value_in_arabic = tradebook.convert_from_roman_to_arabic(value_in_roman)
+
+        assert value_in_arabic == 1650
--- tradebook.py
+++ tradebook.py
@@ -9,3 +9,45 @@ class TradeBook:
             self.traded_items[key] = dict.fromkeys(['value_in_alien', 'value_in_roman', 'value_in_arabic', 'number_of_credits'])
         
         self.conversion_table_alien_to_roman = {}
+
+
+    def convert_from_roman_to_arabic(self, value_in_roman):
+        symbol_to_value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+
+        list_of_roman_characters = list(value_in_roman)
+
+        if len(list_of_roman_characters) == 1:
+            value_in_arabic = symbol_to_value[value_in_roman]
+        else:
+            i = 0
+            sum = 0
+            else_count = 0
+            else_sum = 0
+
+            while i < (len(list_of_roman_characters) - 1):
+                char = list_of_roman_characters[i]
+                next_char = list_of_roman_characters[i + 1]
+
+                if symbol_to_value[char] >= symbol_to_value[next_char]:
+                    if else_count != 0:
+                        sum = sum + (symbol_to_value[char] - else_sum)
+                    else:
+                        sum = sum + symbol_to_value[char]
+                    else_count = 0
+                    else_sum = 0
+                else:
+                    while else_count <= 3:
+                        else_sum = else_sum + symbol_to_value[char]
+                        else_count += 1
+                        break
+                i += 1
+            sum = sum + (symbol_to_value[next_char] - else_sum)
+
+            value_in_arabic = sum
+
+        return value_in_arabic
+
+
+    def convert_from_alien_to_roman(self):
+        pass
+    
