8069faa30aa09f2af085608202ad391d0406cf3b
shweta <nayakshweta07@gmail.com>
2019-03-29 13:07:28 +0530

    Adds code to append result to output file.

--- merchants_of_galaxy.py
+++ merchants_of_galaxy.py
@@ -4,10 +4,18 @@ def get_array_of_lines_from_file(file_path):
     with open(file_path, 'r') as file:
         return file.readlines()
 
+def parse_statement_or_query(line):
+    pass
+
+def append_result_to_output_file(result, file_path):
+    with open(file_path, 'w') as file:
+        return file.write(result)
+
 def main():
     lines = get_array_of_lines_from_file('input_file.txt')
     for line in lines:
-        line.parse_statement_or_query()
+        result = line.parse_statement_or_query()
+        append_result_to_output_file(result, 'output_file.txt')
 
 if __name__ == "__main__":
     main()
